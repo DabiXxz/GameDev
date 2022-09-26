ad32cd96ac03d97b7b756b63bf9bf5d7459378ce
DabiXxz <gabrielhenri22@gmail.com>
2022-09-26 19:17:35 -0300

    Import

---
+++ Game_01/src/com/EtherStudios/world/FloorTile.java
@@ -0,0 +1,11 @@
+package com.EtherStudios.world;
+
+import java.awt.image.BufferedImage;
+
+public class FloorTile extends Tile{
+
+	public FloorTile(int x, int y, BufferedImage sprite) {
+		super(x, y, sprite);
+	}
+
+}
